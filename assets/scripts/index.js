7b740eb457f80d84a12e97effd63388218630089
Brandon Wiley <brandon@blanu.net>
2011-08-21 18:05:22 -0500

    Fixed missing closing paranthesis

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -100,7 +100,7 @@ function checkMobile()
    navigator.userAgent.match(/Android/i) ||
    navigator.userAgent.match(/webOS/i) ||
    navigator.userAgent.match(/iPhone/i) ||
-   navigator.userAgent.match(/iPod/i)
+   navigator.userAgent.match(/iPod/i))
   {
     return true;
   }
